2113f87ece9c846195792d267a3e9a2a3619d241
Mihai <mcosma@gmail.com>
2023-07-07 10:46:05 -0400

    save to steps w/ output

--- ratelimit.sh
+++ ratelimit.sh
@@ -24,4 +24,4 @@ echo "Remaining: $REMAINING"
 echo "Used: $USED"
 echo "The rate limit will reset at $RESET_TIME UTC"
 echo "Time remaining until reset: $TIME_REMAINING_SECONDS seconds ($TIME_REMAINING_MINUTES minutes)"
-echo "remaining=$REMAINING" >> $GITHUB_ENV
+echo "remaining=$REMAINING" >> $GITHUB_OUTPUT
